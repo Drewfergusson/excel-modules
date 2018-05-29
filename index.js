9269974aadeb47e12d7f338d9b52876c031db569
Andrew Ferguson <fergusonandrew4@gmail.com>
2018-05-28 21:14:39 -0700

    bug chasing

--- index.js
+++ index.js
@@ -1,51 +1,8 @@
-// setting undefined because the JS compliler evaluates (null || '') as null
-// also using .split('') because ...String also needs a polyfill on IE which (I think) is the version
-// of iframe browser used on Windows Excel
-const lettersArr = [undefined, ...'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('')];
-const lettersInAlphabet = 26;
+const utils = require('./utils');
 
 module.exports = {
   range,
   rangeFromCell
-}
-
-/**
- *
- * ({startingColumn: String, addedColumns: Number}) => String
- * ('A', 5) => 'F'
- * ('AA' + 1) => 'AB'
- */
-function columnAddition(startingColumn, addedColumns) {
-  return getColumnFromIndex(getIndexFromColumn(startingColumn) + addedColumns);
-}
-
-/**
- * Using the starging column, get the ending column for a row of a given length
- */
-function rowEndingColumn(startingColumn, rowLength) {
-  // addedColumns -1 to get row that includes the starting column in the total length
-  return getColumnFromIndex(getIndexFromColumn(startingColumn) + rowLength - 1);
-}
-
-/**
- * (column: String) => Number
- * ('F') => 5
- * ('AA') => 27
- */
-function getIndexFromColumn(column) {
-  return column.split('').reduce((acc, currentVal) => {
-    return lettersInAlphabet * acc + lettersArr.indexOf(currentVal);
-  }, 0);
-}
-
-// TODO, this needs tests and will likely fail for a third letter
-/**
- * (index: Number) => String
- * (5) => 'F'
- * (27) => 'AA'
- */
-function getColumnFromIndex(index) {
-  return (lettersArr[Math.floor(index / lettersInAlphabet)] || '') + lettersArr[(index % lettersInAlphabet)];
 }
 
 /**
@@ -70,8 +27,8 @@ function parseRange(rangeString) {
     throw new Error('Invalid range string');
   }
 
-  let endingRow = rangeString.match(/:[A-Z]([0-9])+/)? Number(endOfRange.match(/:[A-Z]([0-9])+/)[1]): undefined;
-  let endingColumn = rangeString.match(/:([A-Z])+/)? endOfRange.match(/:([A-Z])+/)[1]: undefined;
+  let endingRow = rangeString.match(/:[A-Z]([0-9])+/)? Number(rangeString.match(/:[A-Z]+([0-9])+/)[1]): undefined;
+  let endingColumn = rangeString.match(/:([A-Z])+/)? rangeString.match(/:([A-Z])+/)[1]: undefined;
 
   return { sheet, startingRow, startingColumn, endingRow, endingColumn };
 }
@@ -81,7 +38,7 @@ function parseRange(rangeString) {
   */
 function rangeFromCell(rangeString, height, width) {
   const expandedRange = this.range(rangeString);
-  expandedRange.addRowsDown(height - 1).addColumnsRight(width - 1);
+  expandedRange.addRowsToStartCell(height - 1).addColumnsToStartCell(width - 1);
   return expandedRange;
 }
 
@@ -103,8 +60,8 @@ function rangeFromCell(rangeString, height, width) {
     endingColumn,
     sheet,
     getLocation,
-    addRowsDown,
-    addColumnsRight,
+    addRowsToStartCell,
+    addColumnsToStartCell,
     rows
   };
 
@@ -127,13 +84,13 @@ function rangeFromCell(rangeString, height, width) {
     return `${this.sheet || ''}!${this.startingColumn}${this.startingRow}:${this.endingColumn || ''}:${this.endingRow || ''}`
   }
 
-  function addRowsDown(number) {
-    this.endingRow = (this.endingRow || this.startingRow)+ number;
+  function addRowsToStartCell(number) {
+    this.endingRow = (this.startingRow) + number;
     return this;
   }
 
-  function addColumnsRight(number) {
-    this.endingColumn = columnAddition(this.endingColumn || this.startingColumn, number);
+  function addColumnsToStartCell(number) {
+    this.endingColumn = utils.columnAddition(this.startingColumn, number);
     return this;
   }
   /**
