ad1994ac4153d12df32e7a6b12b809fe88499b8d
Andrew Ferguson <fergusonandrew4@gmail.com>
2018-06-02 10:34:13 -0700

    version 2.0 changes

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
-const cell = require('./modules/cell');
 const range = require('./modules/range');
 
 module.exports = {
-  range,
-  cell
+  range: rangeBuilder,
+  rangeFrom: () => ({
+    async: () => ({selected, startOfSelected})
+  })
 }
 
- /**
-  * Takes the start of the range and calculates a new range with a given height and width
-  */
-function rangeFromCell(rangeString, height, width) {
-  const expandedRange = this.range(rangeString);
-  expandedRange.addRowsToStartCell(height - 1).addColumnsToStartCell(width - 1);
-  return expandedRange;
+function selected() {
+
 }
 
+function startOfSelected() {
+
+}
 
